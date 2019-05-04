0c509f5a80379462c4e7eaa7c24a4c5c710224a9
John Reeves <49652779+Vader582@users.noreply.github.com>
2019-05-04 12:48:46 -0400

    Add files via upload

---
+++ coding_time.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+neighbor = input("What is your neighbor's name? ")
+
+time = int(input("How many months have you been coding? "))
+time2 = int(input("How many months has your neighbor been coding? "))
+sum = time + time2
+print(f"{name} and {neighbor} have been coding for {sum} total months.")
